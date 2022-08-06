eeddb428d9b5eadf72305d2896619dbad1aa1adf
amr <amr.moheb.sultan@gmail.com>
2022-08-06 20:32:05 +0200

    ahmed byl3ab commit

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // ahmed byl3ab
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
